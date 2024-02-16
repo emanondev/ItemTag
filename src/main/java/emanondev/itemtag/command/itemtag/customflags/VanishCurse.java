c6ce44de159ab915981db20c3fb02d66ec86514f
emanon <emanon_dev@hotmail.it>
2024-02-16 13:00:50 +0100

    + Option to ignore KeepInventory gamerule

--- src/main/java/emanondev/itemtag/command/itemtag/customflags/VanishCurse.java
+++ src/main/java/emanondev/itemtag/command/itemtag/customflags/VanishCurse.java
@@ -20,6 +20,17 @@ public class VanishCurse extends CustomFlag {
 
     @EventHandler(priority = EventPriority.LOW)
     private void event(PlayerDeathEvent event) {
+        if (ItemTag.get().getConfig().loadBoolean("flag.vanishcurse.override_keepinventory", false)) {
+            ItemStack[] inv = event.getEntity().getInventory().getContents();
+            boolean override = false;
+            for (int i = 0; i < inv.length; i++)
+                if (ItemTag.getTagItem(inv[i]).hasBooleanTag(VANISHCURSE_KEY)) {
+                    inv[i] = null;
+                    override = true;
+                }
+            if (override)
+                event.getEntity().getInventory().setContents(inv);
+        }
         event.getDrops().removeIf((item) -> ItemTag.getTagItem(item).hasBooleanTag(VANISHCURSE_KEY));
     }
 
